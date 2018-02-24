8ec5c8dab4b20161910531c1d20a7d3af2b021ea
sunneil <sunneil@hotmail.com>
2018-02-24 12:40:54 +0800

    d

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -301,7 +301,7 @@ public:
 
         bech32_hrp = "bc";
 
-        vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
+       // vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;
